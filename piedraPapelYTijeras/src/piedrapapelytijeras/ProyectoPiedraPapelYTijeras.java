6ec86a4e0159256c0140b974eff0b5cc7f842d94
Pedro Seoane Prado <info@pedroseoaneprado.es>
2022-10-19 09:38:06 +0200

    Renombrado proyecto...

---
+++ piedraPapelYTijeras/src/piedrapapelytijeras/ProyectoPiedraPapelYTijeras.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package piedrapapelytijeras;
+
+import java.util.Scanner;
+
+/**
+ * Piedra, papel y tijera.
+ * 
+ * @author a22pedrovsp
+ * @author a20amelych
+ */
+public class ProyectoPiedraPapelYTijeras {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    
+
+    public static void main(String[] args) {
+        
+
+        Scanner entrada = new Scanner(System.in);
+        String texto = entrada.nextLine();
+        System.out.println(texto);
+
+        // Zona principal
+        System.out.println("Elije piedra, papel o tijeras: ");
+
+
+    }
+    
+}
