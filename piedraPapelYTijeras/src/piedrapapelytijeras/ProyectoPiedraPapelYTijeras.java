064fbb4bd2266df2cbceb77668f5f65dfe15188f
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2022-11-01 17:06:03 +0100

    Mejorado el código, eliminadas redundancias

--- piedraPapelYTijeras/src/piedrapapelytijeras/ProyectoPiedraPapelYTijeras.java
+++ piedraPapelYTijeras/src/piedrapapelytijeras/ProyectoPiedraPapelYTijeras.java
@@ -1,11 +1,10 @@
-
 package piedrapapelytijeras;
 
 import java.util.Scanner;
 
 /**
  * PAPER SCISSORS ROCK!
- * 
+ *
  * @author a22pedrovsp
  * @author a20amelych
  * @version 0.3
@@ -28,9 +27,9 @@ public class ProyectoPiedraPapelYTijeras {
          */
 
         final byte[][] whoWin = {
-                { 1, 2, 0 },
-                { 0, 1, 2 },
-                { 2, 0, 1 }
+                {1, 2, 0},
+                {0, 1, 2},
+                {2, 0, 1}
         };
 
         return whoWin[player][computer];
@@ -38,10 +37,9 @@ public class ProyectoPiedraPapelYTijeras {
     }
 
     /**
-     * Launchs the game, it's uses an exception just in case of bad user input...
+     * Launch's the game, it's uses an exception just in case of bad user input...
      * could be improved
-     * 
-     * TODO due to hard possible debbuging...
+     * TODO due to hard possible debugging...
      */
 
     public static void main(String[] args) {
@@ -63,16 +61,16 @@ public class ProyectoPiedraPapelYTijeras {
         boolean godMode; // It will load sometimes...
         byte scorePlayer = 0;
         byte scorePc = 0;
-        byte playerAction = 0;
+        byte playerAction;
         byte pcAction = 0;
-        String salirdeljuego;
+        String exitGame;
 
         Scanner input = new Scanner(System.in, encoding);
 
         /*
          * Due to be a simple CLI game, we just enter on the main point with a loop,
          * unless when a game it's over the player can choose to halt the game, a
-         * boolean will switch (loopgame) to false.
+         * boolean will switch (loop-game) to false.
          */
         while (loopGame) {
 
@@ -80,7 +78,7 @@ public class ProyectoPiedraPapelYTijeras {
             // not in cmd but it's deprecated who cares.
             System.out.printf("%s%s%s\n", ANSI_PURPLE, mainDialog, ANSI_RESET);
 
-            // @TODO Improve this exception block... just catches everthing...
+            // @TODO Improve this exception block... just catches everything...
             try {
                 playerAction = input.nextByte();
                 input.nextLine();
@@ -92,20 +90,15 @@ public class ProyectoPiedraPapelYTijeras {
                      * We assign the pcAction to always wins to the player!
                      */
                     switch (playerAction) {
-                        case 0:
-                            pcAction = 1;
-                            break;
-                        case 1:
-                            pcAction = 2;
-                            break;
-                        case 2:
-                            pcAction = 1;
-                            break;   
+                        case 0, 2 ->
+                                pcAction = 1; // TIL this... for my taste is hard to read, but I don't like JetBrains warnings...
+                        case 1 -> pcAction = 2;
                     }
-                    System.out.printf("%s\nCHEAT STATUS: %b%s\n", ANSI_RED, godMode, ANSI_RESET);
+
+                    System.out.printf("%s\nCHEAT STATUS: %b%s\n", ANSI_RED, true, ANSI_RESET); // intelliJ hates not using constant when possible...
                 } else {
                     pcAction = (byte) (new java.util.Random().nextInt(3));
-                    System.out.printf("%s\nCHEAT STATUS: %b%s\n", ANSI_GREEN, godMode, ANSI_RESET);
+                    System.out.printf("%s\nCHEAT STATUS: %b%s\n", ANSI_GREEN, false, ANSI_RESET);
                 }
                 System.out.printf("\nThe computer has chosen: %d\n", pcAction);
                 /*
@@ -115,35 +108,34 @@ public class ProyectoPiedraPapelYTijeras {
                  */
 
                 switch (whoWin(playerAction, pcAction)) {
-                    case 0:
+                    case 0 -> {
                         scorePlayer++;
                         System.out.printf("""
-                                %sThe player has won this round...
-                                PLAYER [%d][%d]\n%s
-                                """,
+                                        %sThe player has won this round...
+                                        PLAYER [%d][%d]
+                                        %s
+                                        """,
                                 ANSI_GREEN, scorePlayer, scorePc, ANSI_RESET);
-                        break;
-                    case 1:
-                        System.out.printf("""
-                                %sDraw...
-                                PLAYER [%d][%d]\n
-                                %s""",
-                                ANSI_PURPLE, scorePlayer, scorePc, ANSI_RESET);
-                        break;
-                    case 2:
+                    }
+                    case 1 -> System.out.printf("""
+                                    %sDraw...
+                                    PLAYER [%d][%d]
+                                    %s""",
+                            ANSI_PURPLE, scorePlayer, scorePc, ANSI_RESET);
+                    case 2 -> {
                         scorePc++;
                         System.out.printf("""
-                                %sThe computer has won this round...
-                                PLAYER [%d][%d]\n
-                                %s""",
+                                        %sThe computer has won this round...
+                                        PLAYER [%d][%d]
+                                        %s""",
                                 ANSI_RED, scorePlayer, scorePc, ANSI_RESET);
-                        break;
+                    }
                 }
 
                 if (scorePlayer >= 3 || scorePc >= 3) {
                     System.out.println("Play again? Y/N");
-                    salirdeljuego = input.nextLine();
-                    if (salirdeljuego.toLowerCase().equals("n")) {
+                    exitGame = input.nextLine();
+                    if (exitGame.equalsIgnoreCase("n")) {
                         System.err.println("\n### HALT ###");
                         loopGame = false;
                         input.close();
@@ -153,10 +145,10 @@ public class ProyectoPiedraPapelYTijeras {
                         scorePc = 0;
                     }
                 }
-                
+
             } catch (Exception e) {
                 System.err.printf("%sOnly numbers from 0 to 2 are allowed.%s", ANSI_RED, ANSI_RESET);
-                input.nextLine(); // Limpieza de bufer.
+                input.nextLine(); // Cleaning input buffer
                 java.awt.Toolkit.getDefaultToolkit().beep(); // Alert sound... just from system inherit...
             }
         }
