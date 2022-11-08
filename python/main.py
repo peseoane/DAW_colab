c204fa2eaca6e5b7a73481e000b3ed7658d6dd45
Pedro Vicente Seoane Prado <info@pedroseoaneprado.es>
2022-11-08 16:00:37 +0100

    Clases

--- python/main.py
+++ python/main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import game
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+    pc = game.Player(
+        "pc",
+        10,
+        3
+    )
 
-# Press the green button in the gutter to run the script.
+    print(pc.name, pc.points, pc.score)
+
+    pc.showScore()
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
