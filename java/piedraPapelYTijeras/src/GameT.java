5955340b6d2eb1d37f56e2b4aa98a8ac4adf81d4
amelych <116146886+amelych@users.noreply.github.com>
2022-11-09 10:27:16 +0100

    mejorado clase game

--- java/piedraPapelYTijeras/src/GameT.java
+++ java/piedraPapelYTijeras/src/GameT.java
@@ -1,29 +1,24 @@
 
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class GameT {
     // Atributos
-    private boolean loopGame;
-    private boolean godMode;
-    private byte scorePlayer;
-    private byte scorePc;
-    private byte playerAction;
-    private byte pcAction;
-    private String exitGame;
-
-    Scanner input = new Scanner(System.in);
-
+    private int id;
+    private byte player1;
+    private byte player2;
+    private byte points;
+    private byte wins;
 
     // Constructor
-
-    public GameT(boolean loopGame, boolean godMode, byte playerAction, String exitGame) {
-        this.loopGame = loopGame;
-        this.godMode = godMode;
-        this.scorePlayer = 0;
-        this.scorePc = 0;
-        this.playerAction = playerAction;
-        this.pcAction = 0;
-        this.exitGame = exitGame;
+    
+    public GameT(int id, byte player1, byte player2) {
+        this.id = id;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.points = 0;
+        this.wins = 0;
     }
 
     // Métodos
@@ -43,7 +38,15 @@ public class GameT {
                 """;
     }
 
-    public byte winner(byte playerAction, byte pcAction) {
+    public void stats() {
+        System.out.println("----------------");
+        System.out.println();
+        System.out.println("Ha ganado: " + wins);
+        System.out.println("Lleva " + points + " puntos");
+        System.out.println("----------------");
+    }
+
+    public byte winner(playerT, pcAction) {
         final byte[][] whoWin = {
                 { 1, 2, 0 },
                 { 0, 1, 2 },
@@ -77,6 +80,9 @@ public class GameT {
     }
 
     public void playAgain() {
+        private byte scorePlayer;
+        private byte scorePc;
+        Scanner input = new Scanner(System.in);
         boolean loopGame = true;
         final String exitGame;
         if (scorePlayer >= 3 || scorePc >= 3) {
@@ -93,5 +99,4 @@ public class GameT {
             }
         }
     }
-
 }
