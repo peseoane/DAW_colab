1988ae5ba9558b1103537a6e08086a2c988dd6a2
amelych <116146886+amelych@users.noreply.github.com>
2022-11-10 09:42:31 +0100

    sin acabar

--- java/piedraPapelYTijeras/src/piedrapapelytijeras/Game.java
+++ java/piedraPapelYTijeras/src/piedrapapelytijeras/Game.java
@@ -53,7 +53,7 @@ public class Game {
         System.out.println("----------------");
     }
 
-    public byte getWinner(,  Pc) {
+    public byte getWinner(,) {
         final byte[][] whoWin = {
                 { 1, 2, 0 },
                 { 0, 1, 2 },
